31aa40660959fd63bb21b4099242f5000e9d472c
MrParkerZ7 <neverzero9@gmail.com>
2021-11-06 14:56:54 +0700

    [Update] terraform info

--- main.tf
+++ main.tf
@@ -31,14 +31,14 @@ resource "aws_iam_role" "iam_for_lambda" {
 }
 
 resource "aws_lambda_function" "lambda" {
-  function_name = "hello_lambda"
+  function_name = "terraform_python"
 
   filename         = "${data.archive_file.zip.output_path}"
   source_code_hash = "${data.archive_file.zip.output_base64sha256}"
 
   role    = "${aws_iam_role.iam_for_lambda.arn}"
   handler = "hello_lambda.lambda_handler"
-  runtime = "python3.6"
+  runtime = "python3.9"
 
   environment {
     variables = {
